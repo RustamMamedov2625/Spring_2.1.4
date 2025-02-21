0e46442a41d0810e24f0de2c621f9a8d6d96bc6e
RustamMamedov2625 <mamedovrustam2509@gmail.com>
2025-02-22 00:23:24 +0400

    Task was completed

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -1,15 +1,19 @@
 package koschei.models;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Island2 {
 
-    private Wood3 wood;
 
-    public Island2(Wood3 wood) {
-        this.wood = wood;
+    private final Wood3 wood3;
+
+    public Island2( Wood3 wood3) {
+        this.wood3 = wood3;
     }
 
     @Override
     public String toString() {
-        return ", на острове дерево " + wood.toString();
+        return ", на острове дерево " + wood3.toString();
     }
 }
--- src/main/java/koschei/models/Ocean1.java
+++ src/main/java/koschei/models/Ocean1.java
@@ -1,16 +1,20 @@
 package koschei.models;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Ocean1 {
 
-    @Autowired
-    private Island2 island;
+    private final Island2 island2;
+
+    public Ocean1(@Qualifier("island2") Island2 island2) {
+        this.island2 = island2;
+    }
 
     @Override
     public String toString() {
-        return "на океане остров " + island.toString();
+        return "на океане остров " + island2.toString();
     }
 }
